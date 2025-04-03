b376e70e2ce68694474611aaadebfa8ef7c8e3f2
yyh-gl <yhonda.95.gl@gmail.com>
2025-04-03 10:16:19 +0900

    Update bin/jvm.sh

--- bin/jvm.sh
+++ bin/jvm.sh
@@ -5,4 +5,7 @@
 mkdir $HOME/jvm
 curl \
   -L https://download.oracle.com/java/${JVM_VERSION}/latest/jdk-${JVM_VERSION}_macos-aarch64_bin.tar.gz \
-  -o $HOME/jvm/jdk-${JVM_VERSION}
+  -o $HOME/jvm/jdk-${JVM_VERSION}.tar.gz
+tar -zxvf $HOME/jvm/jdk-${JVM_VERSION}.tar.gz
+mv ./jdk-${JVM_VERSION}*.jdk $HOME/jvm
+rm -rf $HOME/jvm/jdk-${JVM_VERSION}.tar.gz
