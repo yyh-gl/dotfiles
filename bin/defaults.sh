1259a8b786825fadfee9e0c4033236b56dd0dc56
yyh-gl <yhonda.95.gl@gmail.com>
2025-04-05 19:31:41 +0900

    Show Bluetooth and Sound in Control Center

--- bin/defaults.sh
+++ bin/defaults.sh
@@ -7,6 +7,8 @@ defaults write -g com.apple.trackpad.scaling -float 3
 defaults write -g KeyRepeat -int 2
 defaults write -g InitialKeyRepeat -int 15
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults write com.apple.controlcenter 'NSStatusItem Visible Bluetooth' -bool true
+defaults write com.apple.controlcenter 'NSStatusItem Visible Sound' -bool true
 # TODO: Not reflected immediately, so comment out for now
 # defaults write -g AppleInterfaceStyle -string "Dark"
 killall SystemUIServer
