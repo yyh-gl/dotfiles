96b2cddfde614718a29d8c805dff2320b6e39241
yyh-gl <yhonda.95.gl@gmail.com>
2025-04-24 22:26:20 +0900

    Update Emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -349,3 +349,16 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(expand-region yasnippet terraform-mode markdown-mode yaml-mode json-mode php-mode web-mode scala-mode smart-compile flycheck auto-complete)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
