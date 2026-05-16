ebf0ccbc9474193f93cb8ca985ab460eb1461486
yyh-gl <yhonda.95.gl@gmail.com>
2026-05-16 09:29:13 +0900

    refactor(Nix): split home-manager config into separate modules

---
+++ nix/home/dotfiles.nix
@@ -0,0 +1,7 @@
+{ dotfiles, ... }: {
+  home.file.".zpreztorc".source = "${dotfiles}/.zpreztorc";
+  home.file.".config/ghostty/config".source = "${dotfiles}/ghostty-config";
+  home.file.".config/starship.toml".source = "${dotfiles}/starship.toml";
+  home.file.".config/git/config".source = "${dotfiles}/.git-config/config";
+  home.file.".config/git/ignore".source = "${dotfiles}/.git-config/ignore";
+}
