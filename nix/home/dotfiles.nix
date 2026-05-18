c6bbea73ac448e83604a8275ef920d1aad18409e
yyh-gl <yhonda.95.gl@gmail.com>
2026-05-19 07:39:03 +0900

    refactor(Nix): manage karabiner.json via home.file and list-drive symlinks
    - Replace home.activation.karabiner with home.file to remove fixed checkout path dependency
    - Extract Google Drive symlink targets into a list for easier addition and deletion

--- nix/home/dotfiles.nix
+++ nix/home/dotfiles.nix
@@ -1,40 +1,33 @@
-{ dotfiles, lib, config, mode, ... }: {
+{ dotfiles, lib, config, mode, ... }:
+let
+  hd = config.home.homeDirectory;
+  gd = "${hd}/Google Drive/マイドライブ/01_Personal";
+  hobbySymlinks = [
+    { src = "${gd}/00_Engineering";                          dst = "${hd}/Desktop/hobby/00_Engineering"; }
+    { src = "${gd}/01_CasualLife";                           dst = "${hd}/Desktop/hobby/01_CasualLife"; }
+    { src = "${gd}/01_CasualLife/99_Pictures/00_Profile";    dst = "${hd}/Pictures/00_Profile"; }
+    { src = "${gd}/01_CasualLife/99_Pictures/01_Wallpapers"; dst = "${hd}/Pictures/01_Wallpapers"; }
+    { src = "${gd}/01_CasualLife/99_Pictures/02_Geeks";      dst = "${hd}/Pictures/02_Geeks"; }
+    { src = "${gd}/01_CasualLife/99_Pictures/03_IconsForMac"; dst = "${hd}/Pictures/03_IconsForMac"; }
+    { src = "${gd}/01_CasualLife/99_Pictures/04_SlackStamps"; dst = "${hd}/Pictures/04_SlackStamps"; }
+    { src = "${gd}/01_CasualLife/99_Pictures/05_Works";      dst = "${hd}/Pictures/05_Works"; }
+    { src = "${gd}/01_CasualLife/99_Pictures/99_Others";     dst = "${hd}/Pictures/99_Others"; }
+  ];
+in {
   home.file.".config/ghostty/config".source = "${dotfiles}/ghostty-config";
   home.file.".config/starship.toml".source = "${dotfiles}/starship.toml";
   home.file.".config/git/config".source = "${dotfiles}/.git-config/config";
   home.file.".config/git/ignore".source = "${dotfiles}/.git-config/ignore";
-
-  home.activation.karabiner = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    dotfiles="${config.home.homeDirectory}/workspaces/github.com/yyh-gl/dotfiles"
-    mkdir -p "${config.home.homeDirectory}/.config/karabiner"
-    ln -sf "$dotfiles/karabiner.json" \
-      "${config.home.homeDirectory}/.config/karabiner/karabiner.json"
-  '';
+  home.file.".config/karabiner/karabiner.json".source = "${dotfiles}/karabiner.json";
 
   home.activation.desktopSetup = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    ${lib.optionalString (mode == "hobby") ''
-      mkdir -p "${config.home.homeDirectory}/Desktop/hobby"
-      ln -sf "${config.home.homeDirectory}/Google Drive/マイドライブ/01_Personal/00_Engineering" \
-        "${config.home.homeDirectory}/Desktop/hobby"
-      ln -sf "${config.home.homeDirectory}/Google Drive/マイドライブ/01_Personal/01_CasualLife" \
-        "${config.home.homeDirectory}/Desktop/hobby"
-      ln -sf "${config.home.homeDirectory}/Google Drive/マイドライブ/01_Personal/01_CasualLife/99_Pictures/00_Profile" \
-        "${config.home.homeDirectory}/Pictures/00_Profile"
-      ln -sf "${config.home.homeDirectory}/Google Drive/マイドライブ/01_Personal/01_CasualLife/99_Pictures/01_Wallpapers" \
-        "${config.home.homeDirectory}/Pictures/01_Wallpapers"
-      ln -sf "${config.home.homeDirectory}/Google Drive/マイドライブ/01_Personal/01_CasualLife/99_Pictures/02_Geeks" \
-        "${config.home.homeDirectory}/Pictures/02_Geeks"
-      ln -sf "${config.home.homeDirectory}/Google Drive/マイドライブ/01_Personal/01_CasualLife/99_Pictures/03_IconsForMac" \
-        "${config.home.homeDirectory}/Pictures/03_IconsForMac"
-      ln -sf "${config.home.homeDirectory}/Google Drive/マイドライブ/01_Personal/01_CasualLife/99_Pictures/04_SlackStamps" \
-        "${config.home.homeDirectory}/Pictures/04_SlackStamps"
-      ln -sf "${config.home.homeDirectory}/Google Drive/マイドライブ/01_Personal/01_CasualLife/99_Pictures/05_Works" \
-        "${config.home.homeDirectory}/Pictures/05_Works"
-      ln -sf "${config.home.homeDirectory}/Google Drive/マイドライブ/01_Personal/01_CasualLife/99_Pictures/99_Others" \
-        "${config.home.homeDirectory}/Pictures/99_Others"
-    ''}
+    ${lib.optionalString (mode == "hobby") (''
+      mkdir -p "${hd}/Desktop/hobby"
+    '' + lib.concatMapStrings (sl: ''
+      ln -sf "${sl.src}" "${sl.dst}"
+    '') hobbySymlinks)}
     ${lib.optionalString (mode == "work") ''
-      mkdir -p "${config.home.homeDirectory}/Desktop/work"
+      mkdir -p "${hd}/Desktop/work"
     ''}
   '';
 }
