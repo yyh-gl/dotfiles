2b8069fe9652c023eb5916e9968908261c85c1b2
yyh-gl <yhonda.95.gl@gmail.com>
2026-06-05 17:42:47 +0900

    chore(WezTerm): redesign tab bar with arrow style and switch to Ricty Diminished font

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,10 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 local config = wezterm.config_builder()
 
+local SOLID_LEFT_ARROW = utf8.char(0xe0ba)
+local SOLID_LEFT_MOST = utf8.char(0x2588)
+local SOLID_RIGHT_ARROW = utf8.char(0xe0bc)
+
 ---------------------------------
 -- window
 ---------------------------------
@@ -12,10 +16,15 @@ wezterm.on("gui-startup", function(cmd)
 	window:gui_window():maximize()
 end)
 
--- background
 config.colors = {
 	background = "#000000",
 	split = "#5f6527",
+	tab_bar = {
+		background = "#121212",
+		new_tab = { bg_color = "#121212", fg_color = "#FCE8C3", intensity = "Bold" },
+		new_tab_hover = { bg_color = "#121212", fg_color = "#FBB829", intensity = "Bold" },
+		active_tab = { bg_color = "#121212", fg_color = "#FCE8C3" },
+	},
 }
 config.window_background_opacity = 0.7
 config.macos_window_background_blur = 10
@@ -24,31 +33,55 @@ config.window_frame = {
 	active_titlebar_bg = "#000000",
 	inactive_titlebar_bg = "#111111",
 }
+config.use_fancy_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.tab_max_width = 60
+
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-    local background = "#000000"
-    local foreground = "#FFFFFF"
+	local edge_background = "#121212"
+	local background = "#4E4E4E"
+	local foreground = "#1C1B19"
 
-    if tab.is_active then
-        background = "#769ff0"
-        foreground = "#FFFFFF"
-    end
+	if tab.is_active then
+		background = "#769ff0"
+		foreground = "#1C1B19"
+	elseif hover then
+		background = "#FF8700"
+		foreground = "#1C1B19"
+	end
 
-    local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+	local edge_foreground = background
 
-    return {
-        { Background = { Color = background } },
-        { Foreground = { Color = foreground } },
-        { Text = title },
-    }
+	local left_arrow = SOLID_LEFT_ARROW
+	if tab.tab_index == 0 then
+		left_arrow = SOLID_LEFT_MOST
+	end
+
+	local title = " " .. wezterm.nerdfonts.md_robot .. " " .. wezterm.truncate_right(tab.active_pane.title, max_width - 6) .. " "
+
+	return {
+		{ Attribute = { Intensity = "Bold" } },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = left_arrow },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+		{ Attribute = { Intensity = "Normal" } },
+	}
 end)
+
 ---------------------------------
 -- font
 ---------------------------------
 config.font = wezterm.font_with_fallback({
-	"JetBrains Mono",
+	"Ricty Diminished",
 	"Apple Color Emoji",
 })
-config.font_size = 13.5
+config.font_size = 14.5
 
 ---------------------------------
 -- behavior
