f8c72f7436f3caeb029b68c6fd0aa264ca3cc065
yyh-gl <yhonda.95.gl@gmail.com>
2026-06-06 18:42:41 +0900

    perf(Emacs): lazy-load eglot and tune GC percentage at startup
    Remove eager `(require 'eglot)` so eglot is loaded on first use via
    eglot-ensure hooks. Add gc-cons-percentage 0.6 to early-init.el to
    reduce GC frequency during startup.

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -2,5 +2,6 @@
 ;; ▼ GC Optimization
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq gc-cons-threshold most-positive-fixnum)
+(setq gc-cons-percentage 0.6)
 (add-hook 'emacs-startup-hook
           (lambda () (setq gc-cons-threshold (* 16 1024 1024))))
