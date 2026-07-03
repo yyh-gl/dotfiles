085745008ce738b55159363c1be7d4d98cdd0053
yyh-gl <yhonda.95.gl@gmail.com>
2026-07-03 22:51:30 +0900

    feat(Ghostty): manage config file via Nix home-manager

--- nix/home/dotfiles.nix
+++ nix/home/dotfiles.nix
@@ -14,6 +14,7 @@ let
   ];
 in {
   home.file.".config/wezterm/wezterm.lua".source = "${dotfiles}/wezterm.lua";
+  home.file.".config/ghostty/config".source = "${dotfiles}/ghostty-config";
   home.file.".config/starship.toml".source = "${dotfiles}/starship.toml";
   home.file.".config/git/config".source = "${dotfiles}/.git-config/config";
   home.file.".config/git/ignore".source = "${dotfiles}/.git-config/ignore";
